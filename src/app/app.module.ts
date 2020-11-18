c76a701de76574475e101807f2b23c36001def23
willisjk545 <51234717+willisjk545@users.noreply.github.com>
2020-11-18 10:40:19 -0500

    Created Collection page and added dropdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Navigation/header/header.component';
+import { CollectionComponent } from './collection/collection.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: 'collection', component: CollectionComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CollectionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
